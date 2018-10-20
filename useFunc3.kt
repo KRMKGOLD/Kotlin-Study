3e5c623762299a4a573be9f604f4bf1e6e1f2d6d
KRMKGOLD <dkqk0124@naver.com>
2018-10-20 22:41:55 +0900

    modify README, useFunc3

--- useFunc3.kt
+++ useFunc3.kt
@@ -21,7 +21,7 @@ class funClass{
     infix로 중위 표현식을 선언할 시, 함수명을 연산자처럼 사용할 수 있다, 즉 `객체명 함수명 인자값` 이렇게 사용 가능
 */
 
-fun infixMain(args : Array<String>){
+fun infixMain(){
     val obj = funClass();
     obj.infixFun(10)
     obj infixFun 10
@@ -33,3 +33,23 @@ fun infixMain(args : Array<String>){
 }
 
 // 중위 표현식은 클래스 멤버 함수(funClass.infixFun)이거나, 확장 함수(Int.plusFun)일 때만 사용이 가능하다.
+
+/*
+가변 인수(vararg)?
+함수 오버로딩을 통해서 매개변수가 최대 10개까지 선언된 함수를 정의해야 한다고 가정할 때,
+같은 이름을 가진 함수 10개를 선언하게 되면 코드가 길어지는 불편함이 존재한다.
+이 때 함수를 가변 인수를 포함하는 변수로 선언하면 간단하게 작성할 수 있다.
+
+Java에서도 지원하는 기능이나, 더 편리하게 사용할 수 있다고 함.
+ */
+
+fun <T> varargFunc(vararg array : T){
+    for(a in array){
+        println(a)
+    }
+}
+
+fun varargMain(){
+    varargFunc(100, "Kotlin", 100.001)
+    varargFunc(false, true, 20)
+}
